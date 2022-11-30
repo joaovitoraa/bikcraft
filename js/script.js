911f3c80758ac0fc3ef49828070b1a1fca5e8fa3
joaovitoraa <sanvitor2013@gmail.com>
2022-11-30 11:52:25 -0300

    update js

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//ativar Links do Menu//
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    console.log(link);
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+// Ativar Items do Orçamento//
+const paramentros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(elemento);
+}
+
+paramentros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+  console.log(resposta);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
